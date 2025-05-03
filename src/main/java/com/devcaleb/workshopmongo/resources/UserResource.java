a5e7e7369e78ba089c477f24a8504dd89f5aaf44
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2025-05-03 16:03:01 +0200

    Using patterns DTO to returns users

--- src/main/java/com/devcaleb/workshopmongo/resources/UserResource.java
+++ src/main/java/com/devcaleb/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.devcaleb.workshopmongo.resources;
 
+import com.devcaleb.workshopmongo.dto.UserDTO;
 import com.devcaleb.workshopmongo.entities.User;
 import com.devcaleb.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,8 +22,9 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> users = userService.findAll();
-        return ResponseEntity.ok().body(users);
+        List<UserDTO> userDTOS = users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(userDTOS);
     }
 }
