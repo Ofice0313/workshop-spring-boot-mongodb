88a9281e87ce1d21865a45c7419a745286cb2824
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2025-05-03 18:38:11 +0200

    Getting a user by id

--- src/main/java/com/devcaleb/workshopmongo/services/UserService.java
+++ src/main/java/com/devcaleb/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.devcaleb.workshopmongo.services;
 
 import com.devcaleb.workshopmongo.entities.User;
 import com.devcaleb.workshopmongo.repositories.UserRepository;
+import com.devcaleb.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll(){
        return userRepository.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
     }
 }
---
+++ src/main/java/com/devcaleb/workshopmongo/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.devcaleb.workshopmongo.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String msg){
+        super(msg) ;
+    }
+}
