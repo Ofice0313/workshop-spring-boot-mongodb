efde65086cb53a6aa60c10fccdae06bc4aa51379
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2025-05-03 14:31:14 +0200

    Project created

---
+++ src/main/java/com/devcaleb/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.devcaleb.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
